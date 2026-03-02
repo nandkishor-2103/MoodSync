6d0d8ce5fda6c4540fe41c5ccfd33fa1111ce63f
nandkishor-2103 <mandalnandkishorbk@gmail.com>
2026-03-02 20:09:35 +0530

    implements redis for token blacklist after user logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const userModel = require('../models/user.model');
 const blacklistModel = require('../models/blacklist.model');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const redis = require('../config/cache');
 
 async function registerUser(req, res) {
   const { username, email, password } = req.body;
@@ -102,9 +103,7 @@ async function logoutUser(req, res) {
 
   res.clearCookie('token');
 
-  await blacklistModel.create({
-    token,
-  });
+  await redis.set(token, Date.now().toString(), 'EX', 60 * 60);
 
   res.status(200).json({
     message: 'user logout successfully',
